77feca6e8a016acf770b5e6ac665b433d1a72b08
Gopal <Gopaal@gmail.com>
2024-11-25 18:06:41 -0500

    initial commit

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+    type = string
+    default = "ami-06b21ccaeff8cd686"
+  
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "instance_name" {
+    type = string
+    default = "test_server"
+}
